de43e61bf078f7079815645fc3ffffe4ce6a1531
Faith Walker <faithcorinne@gmail.com>
2017-12-10 12:41:59 -0500

    Search cards style updates
    Added styling to space listing cards according to designs (primarily desktop).

--- src/components/Space/SpaceResultItem.js
+++ src/components/Space/SpaceResultItem.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 const SpaceResultItem = () => {
 	return (
-		<div className="spaceListItem col-xs-12 col-sm-6 col-md-6">
-			<div>
-                <img src={require('../../assets/studio.jpg')} />
+            <div className="spaceListItem col-xs-12 col-sm-6 col-md-6">
+                <div>
+                    <img src={require('../../assets/paint.jpg')} />
+                </div>
+
+                <div className="spaceCardDetails">
+                    <h3 className="spaceCardName">Pearl Studios</h3>
+                    <h4 className="spaceCardLocation">Georgetown, Washington D.C.</h4>
+                    <p className="spaceCardIcons">(icons go here)</p>
+                </div>
+                <div className="spaceCardPricing">
+                    <p className="pricingCost">$30</p>
+                </div>
+
             </div>
-            <div>
-                <h3 className="spaceCardName">Pearl Studios</h3>
-            </div>
-            <div>
-                <h4 className="spaceCardLocation">Georgetown, Washington D.C.</h4>
-            </div>
-            <div>
-                <p>(icons go here)</p>
-            </div>
-		</div>
 	);
 };
 
